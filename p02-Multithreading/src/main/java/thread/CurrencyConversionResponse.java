aed2ae795902efb06c1d24ae2758c6957b25b7e4
UL <92898729+GTmhyu@users.noreply.github.com>
2023-05-07 18:58:15 +0700

    Add files via upload

---
+++ p02-Multithreading/src/main/java/thread/CurrencyConversionResponse.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thread;
+
+import java.util.Map;
+import java.util.Map; import java.util.TreeMap;
+
+public class CurrencyConversionResponse {
+private String base;
+private String date;
+private Map<String, String> rates = new TreeMap<>();
+
+public Map<String, String> getRates () {
+
+return rates;
+}
+public void setRates (Map<String, String> rates) { this.rates = rates;
+}
+public String getBase() {
+
+return base;
+}
+public void setBase (String base) {
+
+this.base = base;
+}
+public String getDate() {
+
+return date;
+}
+public void setDate(String date) {
+this.date = date;
+}
+}
